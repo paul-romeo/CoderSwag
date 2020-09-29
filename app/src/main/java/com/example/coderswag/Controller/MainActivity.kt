2db6cc38099211864d5cc28f64057e340eb18d3e
paul-romeo <countmedream@yahoo.com>
2020-09-28 21:32:07 -0700

    refactored for recycler view adapter

--- app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -4,28 +4,38 @@ import android.R.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.coderswag.Adapters.CategoryAdapter
+import com.example.coderswag.Adapters.CategoryRecycleAdapter
 import com.example.coderswag.Model.Category
 import com.example.coderswag.R
 import com.example.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var adapter: CategoryAdapter
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+//        adapter = CategoryAdapter(this, DataService.categories)
+//        adapter = CategoryRecycleAdapter(context = this, categories = DataService.categories)
+//        CategoryRecycleAdapter(val context: Context, val categories: List<Category>)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
 
         // Display message whenever you click on a category
 //        categoryListView.setOnItemClickListener { adapterView, view, i, l ->
 //            val category = DataService.categories[i]
 //            Toast.makeText(this, "You clicked on the ${category.title} cell", Toast.LENGTH_SHORT).show()
-        }
     }
 }
