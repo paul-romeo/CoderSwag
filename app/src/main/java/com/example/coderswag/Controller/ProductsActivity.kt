c3d79a3b96304033b10f5854ea151d34b8f88841
paul-romeo <countmedream@yahoo.com>
2020-09-30 15:43:00 -0700

    lesson 6

--- app/src/main/java/com/example/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/ProductsActivity.kt
@@ -1,16 +1,34 @@
 package com.example.coderswag.Controller
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.coderswag.Adapters.ProductsAdapter
 import com.example.coderswag.R
+import com.example.coderswag.Services.DataService
 import com.example.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+    lateinit var adapter: ProductsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
-        val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)  // for debugging
+        val categoryType= intent.getStringExtra(EXTRA_CATEGORY)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType.toString()))
+
+        val orientation = resources.configuration.orientation
+        var spanCount = 2
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) spanCount = 3
+
+        var screenSize = resources.configuration.screenWidthDp
+        if (screenSize > 720) spanCount = 3 // for tablet size
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
     }
 }
